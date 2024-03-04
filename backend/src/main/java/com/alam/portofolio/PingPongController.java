c011a8380fc19ec9e971d7e27caa0f847a29aca8
alamsn <28.alamnugroho@gmail.com>
2024-03-05 05:13:30 +0700

    add ping pong counter

--- backend/src/main/java/com/alam/portofolio/PingPongController.java
+++ backend/src/main/java/com/alam/portofolio/PingPongController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PingPongController {
 
+    private static int COUNTER = 0;
     private record PingPong(String result) {}
 
     @GetMapping("/ping")
     private PingPong getPingPong() {
-        return new PingPong("Pong Pong Pong");
+        return new PingPong("Pong: %s".formatted(++COUNTER));
     }
 }
