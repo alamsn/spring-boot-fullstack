d56d88214022fbea1d39ba120f1e5e8db3d05295
alamsn <28.alamnugroho@gmail.com>
2024-03-04 19:19:06 +0700

    CD Workflow - add slack integration

--- backend/src/main/java/com/alam/portofolio/PingPongController.java
+++ backend/src/main/java/com/alam/portofolio/PingPongController.java
@@ -10,6 +10,6 @@ public class PingPongController {
 
     @GetMapping("/ping")
     private PingPong getPingPong() {
-        return new PingPong("Pong Pong Pong");
+        return new PingPong("Pong Pong");
     }
 }
