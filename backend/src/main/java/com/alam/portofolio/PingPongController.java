9d973c05b55a7ac18c6e1fe12e2c224f1bd5ebad
alamsn <28.alamnugroho@gmail.com>
2024-03-04 08:04:17 +0700

    CD Workflow with new controller

---
+++ backend/src/main/java/com/alam/portofolio/PingPongController.java
@@ -0,0 +1,15 @@
+package com.alam.portofolio;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+
+    private record PingPong(String result) {}
+
+    @GetMapping("/ping")
+    public PingPong getPingPong() {
+        return new PingPong("Pong");
+    }
+}
