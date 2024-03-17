75fdad7b05da52b6c4404fe4c57802cc99b26e88
alamsn <28.alamnugroho@gmail.com>
2024-03-17 14:29:19 +0700

    test deploy frontend

--- .ci/build-publish.sh
+++ .ci/build-publish.sh
@@ -1,11 +1,13 @@
+# Memeriksa apakah variabel USERNAME, REPO, dan TAG telah diatur
 : "${USERNAME:?USERNAME not set or empty}"
 : "${REPO:?REPO not set or empty}"
 : "${TAG:?TAG not set or empty}"
 
-docker build . \
-  --platform=linux/amd64, linux/arm64 \
+# Build image Docker menggunakan buildx
+docker buildx build \
+  --platform=linux/amd64,linux/arm64 \
   -t "${USERNAME}/${REPO}:${TAG}" \
   -t "${USERNAME}/${REPO}:latest" \
-  "@{@:2}" \
+  . \
   --push \
-  "$1"
+  --build-arg api_base_url=$api_base_url
