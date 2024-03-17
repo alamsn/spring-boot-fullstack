00530acd997a0f25550da54978b4fdc3e6c5d3f7
alamsn <28.alamnugroho@gmail.com>
2024-03-17 14:58:08 +0700

    test deploy frontend

--- .ci/build-publish.sh
+++ .ci/build-publish.sh
@@ -3,11 +3,14 @@
 : "${REPO:?REPO not set or empty}"
 : "${TAG:?TAG not set or empty}"
 
-# Build image Docker menggunakan buildx
+# Build image Docker menggunakan build
 docker build \
   --platform=linux/amd64 \
   -t "${USERNAME}/${REPO}:${TAG}" \
   -t "${USERNAME}/${REPO}:latest" \
   . \
-  --push \
-  --build-arg api_base_url=$api_base_url
+  --build-arg api_base_url=http://alamsn-fs.ap-southeast-1.elasticbeanstalk.com:8080
+
+# Push image ke registry Docker
+docker push "${USERNAME}/${REPO}:${TAG}"
+docker push "${USERNAME}/${REPO}:latest"
